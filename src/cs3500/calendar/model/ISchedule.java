d54c327ed4eec64148067818a9611bf72e05789e
jackbmerrill <jackbmerrilliv@gmail.com>
2024-03-14 10:39:45 -0400

    Updates to schedule

--- src/cs3500/calendar/model/ISchedule.java
+++ src/cs3500/calendar/model/ISchedule.java
@@ -10,54 +10,61 @@ public interface ISchedule {
   /**
    * Adds an event to the schedule. If there is a time conflict for the host, throws exception.
    * //TODO: how do we handle invalid add for an attending user
+   *
    * @param event an event to be added to the schedule
    * @throws IllegalStateException if there is already an event at the same time
    */
-  public void addEvent(Event event) throws IllegalStateException;
+  void addEvent(Event event) throws IllegalStateException;
 
   /**
    * To remove an event from the schedule. Removes the current user from the event
    * and removes the event from this schedule.
+   *
    * @param eventName an event to be removed from the schedule
    * @throws IllegalStateException if no such event exists in this schedule
    */
-  public void removeEvent(String eventName);
+  void removeEvent(String eventName);
 
   /**
    * To modify the event name
+   *
    * @param eventName the event on the schedule name to be changed
    * @throws IllegalStateException if no such event exists
    */
-  public void modifyEventName(String eventName, String name);
+  void modifyEventName(String eventName, String name);
 
   /**
    * To modify the event time.
+   *
    * @param eventName an event name on the schedule to be changed
    * @throws IllegalStateException if no such event exists or event already exists at given time
    */
-  public void modifyEventTime(String eventName, Time time);
+  void modifyEventTime(String eventName, Time time);
 
   /**
    * To modify the event location.
+   *
    * @param eventName an event location to be changed on the schedule
    * @throws IllegalStateException if no such event exists
    */
-  public void modifyEventLocation(String eventName, Location location);
+  void modifyEventLocation(String eventName, Location location);
 
   /**
    * To return all the user of the event.
+   *
    * @param eventName an event to get users of
    * @return the event users of the requested event as a list of string
    * @throws IllegalStateException if no such event exists
    */
-  public List<String> getAllEventUsers(String eventName);
+  List<String> getAllEventUsers(String eventName);
 
   /**
    * Returns the requested event as a copy.
+   *
    * @param eventName the name of the desired event.
    * @return the requested event as a new object
    * @throws IllegalStateException if no such event exists
    */
-  public Event getEvent(String eventName);
+  Event getEvent(String eventName);
 
 }
