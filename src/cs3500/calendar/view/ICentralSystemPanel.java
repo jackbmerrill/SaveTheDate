a8e839479bab6750f4b9ad4d3a4c9c670586c9b1
milomargolis <margolis.mi@northeastern.edu>
2024-03-26 12:37:19 -0400

    Added central system class

--- src/cs3500/calendar/view/ICentralSystemPanel.java
+++ src/cs3500/calendar/view/ICentralSystemPanel.java
@@ -4,4 +4,49 @@ package cs3500.calendar.view;
  * To represent the central system panel's capabilities.
  */
 public interface ICentralSystemPanel {
+
+  /**
+   * Makes the view visible to the user. Shows itself, the current user's event frame.
+   */
+  void makeVisible();
+
+  /**
+   * Signal the view to draw itself and updates the view.
+   */
+  void refresh();
+
+  /**
+   * To select the user's calendar to view.
+   */
+  void selectUser();
+
+  /**
+   * To create a new event.
+   */
+  void createEvent();
+
+  /**
+   * To schedule an event.
+   */
+  void scheduleEvent();
+
+  /**
+   * To add a calendar to the central system.
+   */
+  void addCalendar();
+
+  /**
+   * To save a calendar to the central system.
+   */
+  void saveCalendar();
+
+  /**
+   * To select an event.
+   */
+  void selectEvent();
+
+
+
+
+
 }
